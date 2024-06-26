347558ca4a30d24de8485d02284bcb6933b3f6bd
Joel Roy <144830771+Joelkgcas@users.noreply.github.com>
2024-06-26 09:38:39 +0530

    Add files via upload

---
+++ task_day2/app.js
@@ -0,0 +1,79 @@
+// •    Store the following into variables: geographic location, job title.var geographiclocation = "thekkady";
+var geographiclocation = "thekkady";
+var jobtitle= "web develpoer ";
+console.log(jobtitle,geographiclocation);
+
+
+//On a single line, declare three variables with the given names and values: firstName = "Joel" lastName = "Roy" age = 20.
+
+var firstName="Joel", lastName="Roy",age=20;
+console.log(firstName,lastName,age);
+
+//  Age Calculator
+let birthYear = 2004;
+let futureYear = 2025;
+let currentAge = futureYear - birthYear;
+let nextAge = currentAge + 1;
+console.log(`I will be either ${currentAge} or ${nextAge} in ${futureYear}`);
+
+//Ever wonder how much a "lifetime supply" of your favorite snack is? Wonder no more!
+let currentage = 20;
+let maximumAge = 80;
+let amountPerDay = 2;
+let remainingYears = maximumAge - currentage;
+let snacksPerYear = amountPerDay * 365;
+let totalSnacksNeeded = snacksPerYear * remainingYears;
+console.log(`You will need ${totalSnacksNeeded} snacks to last you until the ripe old age of ${maximumAge}.`);
+
+
+
+
+
+
+
+
+
+
+
+// task1
+var petDog="REX";
+
+
+// task2
+var petCat="pepper";
+
+
+// task3
+console.log(petDog);
+
+
+// task4
+console.log(petCat);
+
+// task5
+console.log("my petDog's name is:",petDog);
+
+// task6
+console.log("my petCat's name is:",petCat);
+
+// task7
+var catSound="purr";
+
+// task8
+var dogSound="woof";
+
+// task9
+
+console.log(petDog ,"says",dogSound);
+
+// task10
+console.log(petCat ,"says",catSound);
+
+// task11
+
+
+
+
+// task12
+
+console.log(petCat ,"now says",catSound);
